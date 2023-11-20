48139c17009c64749c8829037b9f4979c382c5ca
Simone Micheletti <86918431+SimoneMic@users.noreply.github.com>
2023-11-20 18:08:10 +0100

    Update build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -1,3 +1,3 @@
  #!/bin/bash         
 cd $PWD
-docker build . --build-arg "GIT_USERNAME=" --build-arg "GIT_USER_EMAIL=" -t simonemiche/ergocub_nav_base:test_v1 -f Dockerfile
+docker build . --build-arg "GIT_USERNAME=" --build-arg "GIT_USER_EMAIL=" -t <name/your_tag_here> -f Dockerfile
