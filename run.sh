f74ead601667a6d56df6050c7dfc56cc804b20a0
Simone Micheletti <simone_micheletti@outlook.it>
2024-04-02 15:26:11 +0200

    added run script

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+NAME=simonemiche/ergocub_nav_base
+TAG=tmp
+
+sudo xhost +
+sudo docker run \
+     --network=host --privileged \
+     -it \
+     --rm \
+     -e DISPLAY=unix${DISPLAY} \
+     --device /dev/dri/card0:/dev/dri/card0 \
+     -v /tmp/.X11-unix:/tmp/.X11-unix \
+     ${NAME}:${TAG} bash
+
+     #-v /home/simomic/rosbag2_2024_01_11-16_35_16:/home/ecub_docker/rosbags \
