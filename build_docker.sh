1e84e383f1ceb126135bf7c58d93e6129ff7e9b8
ergocub <ergocub@iit.it>
2026-02-12 20:18:08 +0100

    updated setup and config for robot

--- build_docker.sh
+++ build_docker.sh
@@ -1,3 +1,3 @@
  #!/bin/bash         
 cd $PWD
-docker build . --build-arg "GIT_USERNAME=$1" --build-arg "GIT_USER_EMAIL=$2" -t simonemiche/ergocub_nav_base:ergocubSN001 -f Dockerfile
+docker build . --build-arg "GIT_USERNAME=$1" --build-arg "GIT_USER_EMAIL=$2" -t simonemiche/ergocub_nav_base:ergocubSN001_iron -f Dockerfile_iron
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 NAME=simonemiche/ergocub_nav_base
-TAG=ergocubSN002     
+TAG=ergocubSN001     
 
 sudo xhost +
 
@@ -31,13 +31,45 @@ else
      exit 1
 fi
 
-sudo docker run \
-     --network=host --privileged \
+CONTAINER_NAME="ecub_docker"
+IMAGE_NAME=${NAME}:${TAG}
+MACVLAN_NET="macvlan_net"
+MACVLAN_PARENT="enp45s0"
+CONTAINER_IP="10.0.2.50"
+HOST_SSH_KEY="$HOME/.ssh/id_ed25519.pub"
+CONTAINER_USER="ecub_docker"
+
+# Generate ssh key
+#if [ ! -f "${HOST_SSH_KEY}" ]; then
+#    echo "SSH key not found, generating one..."
+#    ssh-keygen -t ed25519 -f "${HOME}/.ssh/id_ed25519" -N "" -C "$CONTAINER_USER"
+#fi
+#echo "SSH key found: ${HOST_SSH_KEY}"
+
+# Maclavan network for isolating the container with its own IP
+#if ! docker network ls | grep -q "${MACVLAN_NET}"; then
+#    echo "Creating macvlan network ${MACVLAN_NET}..."
+#    docker network create -d macvlan \
+#        --subnet=10.0.2.0/24 \
+#        --gateway=10.0.2.1 \
+#        -o parent=${MACVLAN_PARENT} \
+#        ${MACVLAN_NET}
+#fi
+
+# create container
+sudo docker run --rm\
+     --name ${CONTAINER_NAME} \
+     --network=host \
+     --privileged \
      -it \
-     --rm \
      -e DISPLAY=unix${DISPLAY} \
      --device /dev/dri/card0:/dev/dri/card0 \
      -v /tmp/.X11-unix:/tmp/.X11-unix \
      -v $YARP_CONF_PATH:/home/ecub_docker/.config/yarp/yarp.conf \
      -v $CYCLONE_PATH:/home/ecub_docker/cyclonedds.xml \
-     ${NAME}:${TAG} bash
+     -e YARP_ROBOT_NAME=${YARP_ROBOT_NAME} \
+     -v /media/ergocub/OS/rosbags:/home/ecub_docker/rosbags \
+     ${IMAGE_NAME}
+    bash
+#--ip ${CONTAINER_IP} \
+#-v ${HOST_SSH_KEY}:/home/ecub_docker/.ssh/authorized_keys \
